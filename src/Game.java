6ea78f347791af1fd238709b44b09317975eb701
elgem <elgemytb@gmail.com>
2021-12-15 18:48:03 +0100

    Logique du jeu fonctionnement pas totalement => problème de contage des cellules alentours

--- src/Game.java
+++ src/Game.java
@@ -15,6 +15,7 @@ public class Game {
     private boolean[][] cellGrid;
 
     private int gameSpeed = 1;
+    private int yOffset;
 
     public Game(GameCanvas gameCanvas) {
         this.gameCanvas = gameCanvas;
@@ -23,6 +24,8 @@ public class Game {
 
         cellGrid = new boolean[NUMBER_ROW][NUMBER_COL];
         cellGrid[5][5] = true;
+        cellGrid[5][6] = true;
+        cellGrid[5][7] = true;
     }
 
     public void putCell(int x, int y) {
@@ -65,7 +68,8 @@ public class Game {
     }
 
     public void update() {
-
+        //checkCells();
+        System.out.println(countSurroundingCells(6, 4));
     }
 
     public void render() {
@@ -82,5 +86,59 @@ public class Game {
 
     public boolean[][] getCellGrid() {
         return cellGrid;
+    }
+
+    private void checkCells() {
+        for (int y = 0; y < cellGrid.length; y++) {
+            for (int x = 0; x < cellGrid[0].length; x++) {
+                int numberCells = countSurroundingCells(x, y);
+                System.out.println(numberCells);
+                if (isAlive(x, y)) {
+                    if (numberCells != 2 && numberCells != 3) {
+                        cellGrid[y][x] = false;
+                    }
+                } else {
+                    if (numberCells == 3) {
+                        cellGrid[y][x] = true;
+                    }
+                }
+            }
+
+        }
+    }
+
+    private int countSurroundingCells(int x, int y) {
+        int aliveCellsCounter = 0;
+
+        for (int yOffset = -1; yOffset <= 1; yOffset++) {
+            for (int xOffset = -1; xOffset <= 1; xOffset++) {
+
+                if (xOffset != 0 && yOffset != 0) {
+                    int searchX = x + xOffset;
+                    int searchY = y + yOffset;
+
+                    if (isInGrid(searchX, searchY)) {
+                        if (isAlive(searchX, searchY)) {
+                            aliveCellsCounter++;
+                        }
+                    }
+                }
+
+            }
+        }
+
+        return aliveCellsCounter;
+
+    }
+
+    private boolean isInGrid(int x, int y) {
+        boolean yCorrect = x >= 0 && x < cellGrid.length;
+        boolean xCorrect = y >= 0 && y < cellGrid[0].length;
+
+        return xCorrect && yCorrect;
+    }
+
+    private boolean isAlive(int x, int y) {
+        return cellGrid[y][x];
     }
 }
